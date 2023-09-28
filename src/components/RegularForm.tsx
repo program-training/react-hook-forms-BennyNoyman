cc664c9a00e3ac1a7fca550f35c505a45e081da9
BennyNoyman <bn0504127633@gmail.com>
2023-09-28 10:42:01 +0300

    changed to react hook form

--- src/components/RegularForm.tsx
+++ src/components/RegularForm.tsx
@@ -1,63 +1,51 @@
-import  { useState, ChangeEvent, FormEvent } from 'react';
+import { useForm, SubmitHandler } from "react-hook-form";
 
 interface FormData {
   username: string;
   email: string;
   password:string;
 }
-
+const onSubmit: SubmitHandler<FormData> = data => alert(JSON.stringify(data));
 function RegularForm() {
-  const [formData, setFormData] = useState<FormData>({
-    username: '',
-    email: '',
-    password: '',
-  });
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    alert(JSON.stringify(formData));
-  };
-
+    const { register, formState: { errors }, handleSubmit } = useForm<FormData>();
   return (
-    <form onSubmit={handleSubmit}>
-        <h1>Change Me To React Hook Form</h1>
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>I Have Been Changed To React Hook Form</h1>
       <div>
         <input
           type="text"
           id="username"
-          name="username"
+          //name="username"
           placeholder='Enter UserName'
-          value={formData.username}
-          onChange={handleChange}
+          {...register("username", {required: true, minLength: 2})}
+          aria-invalid={errors.username ? "true" : "false"}
         />
+          {errors.username?.type === 'required' && <p role="alert">User name is required</p>}
+          {errors.username?.type === 'minLength' && <p role= "alert">Minimum 2 chars</p>}
       </div>
       <div>
         <input
           type="text"
           id="email"
-          name="email"
           placeholder='Enter Email'
-          value={formData.email}
-          onChange={handleChange}
+          {...register("email", {required: true, pattern: /^\S+@\S+$/i})}
+          aria-invalid={errors.email ? "true" : "false"}
         />
+          {errors.email?.type === 'required' && <p role="alert">Email is required</p>}
+          {errors.email?.type === 'pattern' && <p role= "alert" >Stick to Email pattern</p>}
       </div>
       <div>
         <input
           type="text"
           id="password"
-          name="password"
           placeholder='Enter Password'
-          value={formData.password}
-          onChange={handleChange}
+          {...register("password", {required: true, minLength: 8, maxLength: 20, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/})}
+          aria-invalid={errors.password ? "true" : "false"}
         />
+          {errors.password?.type === 'required' && <p role="alert">Password is required</p>}
+          {errors.password?.type === "minLength" && <p role="alert">Minimum 8 chars</p>}
+          {errors.password?.type === "maxLength" && <p role="alert">Max 20 chars</p>}
+          {errors.password?.type === 'pattern' && <p role= "alert">Password must contain upper case letter, lower case letter, digit and a special char</p>}
       </div>
       <button type="submit">Submit</button>
     </form>
